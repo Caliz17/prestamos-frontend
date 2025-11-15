dec158105970aceb6c4c3c733b637fb7fff70ab8
Isaac Caliz <isaaccaliz305@gmail.com>
2025-11-14 21:48:33 -0600

    crud clientes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Front\ClienteController as ClienteController;
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::get('reset-password/{token}', function (Request $request, $token) {
     $email = $request->query('email');
@@ -15,21 +20,24 @@ Route::get('reset-password/{token}', function (Request $request, $token) {
         'token' => $token,
         'email' => $email
     ]);
-})->name('password.reset-custom'); // Usa un nombre diferente
-
-// Luego la ruta principal
-Route::get('/', function () {
-    return redirect()->route('login');
-});
+})->name('password.reset-custom');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Perfil de usuario
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
+    // Clientes 
+    Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
+    Route::get('/clientes/create', [ClienteController::class, 'create'])->name('clientes.create');
+    Route::post('/clientes', [ClienteController::class, 'store'])->name('clientes.store');
+    Route::get('/clientes/{id}/edit', [ClienteController::class, 'edit'])->name('clientes.edit');
+    Route::put('/clientes/{id}', [ClienteController::class, 'update'])->name('clientes.update');
+    Route::delete('/clientes/{id}', [ClienteController::class, 'destroy'])->name('clientes.destroy');
+});
 require __DIR__.'/auth.php';
